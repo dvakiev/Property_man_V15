6dcd46c2caa3236f93dd84e3b954dab755b6e135
Dmytro Pavlov <dmytro.pavlov@jetsoftpro.com>
2022-08-23 22:39:08 +0300

    [REF] Refactor cron job

--- erpbox_property_management/models/product_template.py
+++ erpbox_property_management/models/product_template.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from odoo.exceptions import UserError
 
 
 days = list(map(str, range(1, 32)))
@@ -18,18 +19,22 @@ class ProductTemplate(models.Model):
         current_day = str(today.day)
         products = self.env["product.product"].search([
             ("auto_validation", "=", current_day)])
+
         for product in products:
             tenants = self.env["tenant.details"].search([
                 ("state", "=", "in_progress"),
                 ("product_id", "=", product.id)])
+
             for tenant in tenants:
+                check_balance = sum(self.env["account.move.line"].search([
+                        ("partner_id", "=", tenant.partner_id.id)
+                    ]).mapped("balance")) * -1
                 rds = tenant.rent_details_ids.filtered(
-                    lambda r: r.date <= today and r.payment_state != "paid")
+                    lambda r: r.date <= today and \
+                    r.invoice_id.state != "posted")
+
                 for rd in rds:
                     rd.invoice_id.action_post()
-                    check_balance = sum(self.env["account.move.line"].search([
-                            ("partner_id", "=", tenant.partner_id.id)
-                        ]).mapped("balance")) * -1
-                    if check_balance > 0:
+                    if check_balance > 0 and rd.payment_state != "paid":
                         wizard = self.env["account.payment.register"].create({})
                         wizard.action_create_payments()
