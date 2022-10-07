ad69d296b98e47bc540a26042d2973af8982be8e
dvakiev <39129598+dvakiev@users.noreply.github.com>
2022-10-07 17:21:57 +0300

    catalog_auto

---
+++ catalog_auto/__manifest__.py
@@ -0,0 +1,31 @@
+{
+    'name': 'Catalog auto',
+    'version': '15.0.0.0.3',
+    'license': 'Other proprietary',
+    'category': 'Technical Settings',
+
+    'summary': 'Extend product management',
+
+    'complexity': 'easy',
+    'sequence': 3,
+
+    'author': 'Kitworks Systems',
+    'website': 'https://kitworks.systems/',
+
+    'depends': [
+        'stock', 'product_brand_inventory',
+    ],
+
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'views/catalog_auto_view.xml',
+        'views/product_brand_view.xml',
+        'views/product_template_view.xml',
+        'views/menu_items.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
